5c6bb6d3385860668844b58c154365bddd0473dd
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2022-08-28 14:47:11 +0300

    feat: add functions that counts general count of green, brown, blue cards

--- js/script.js
+++ js/script.js
@@ -15,3 +15,46 @@ export const displayAncients = () => {
 };
 
 displayAncients();
+
+//-------------------------COUNT GENERAL COUNT OF GRENN, BROWN, BLUE CARDS-------------------------------
+
+const ancientsСontainer = document.querySelector('.ancients-container');
+const difficultyContainer = document.querySelector('.difficulty-container');
+let targetElementAncientCard;
+let countOfGreenCards;
+let countOfBlueCards;
+let countOfBrownCards;
+let countOfGreenCardsFromAnciant;
+let countOfBrownCardsFromAnciant;
+let countOfBlueCardsFromAnciant;
+let idOfAncient;
+
+//counts general count Of green cards
+const getCountOfGreenCardsFromAnciant = (idOfAncient) => {
+    for (let i = 0; i < ancientsData.length; i++) {
+        if (ancientsData[i].id === idOfAncient) {
+            countOfGreenCards = ancientsData[i].firstStage.greenCards + ancientsData[i].secondStage.greenCards + ancientsData[i].thirdStage.greenCards;
+            return countOfGreenCards;
+        }
+    }
+}
+
+//counts general count Of brown cards
+const getCountOfBrownCardsFromAnciant = (idOfAncient) => {
+    for (let i = 0; i < ancientsData.length; i++) {
+        if (ancientsData[i].id === idOfAncient) {
+            countOfBrownCards = ancientsData[i].firstStage.brownCards + ancientsData[i].secondStage.brownCards + ancientsData[i].thirdStage.brownCards;
+            return countOfBrownCards;
+        }
+    }
+}
+
+//counts general count Of blue cards
+const getCountOfBlueCardsFromAnciant = (idOfAncient) => {
+    for (let i = 0; i < ancientsData.length; i++) {
+        if (ancientsData[i].id === idOfAncient) {
+            countOfBlueCards = ancientsData[i].firstStage.blueCards + ancientsData[i].secondStage.blueCards + ancientsData[i].thirdStage.blueCards;
+            return countOfBlueCards;
+        }
+    }
+}
